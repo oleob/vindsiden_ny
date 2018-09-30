1d1fa38a56fa7786d424050891d78b404af23ba1
Ole Barsch <ole.barsch@gmail.com>
2018-09-30 14:41:11 +0200

    Add About

---
+++ src/containers/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="container">
+      <h1>Om oss</h1>
+      <p>Denne siden benytter seg av API-et til</p>
+      <a href="https://vindsiden.no">vindsiden.no</a>.
+      <p>
+        Ved spørsmål eller forbedringsforslag send en mail til
+        ole.barsch@gmail.com.
+      </p>
+    </div>
+  );
+};
+
+export default About;
--- src/containers/Routes.jsx
+++ src/containers/Routes.jsx
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 
 import Home from './Home';
 import Station from './Station';
+import About from './About';
 import NavBar from '../components/NavBar';
 
 const Routes = () => (
@@ -14,9 +15,13 @@ const Routes = () => (
           <Home />
         </Route>
         <Route
+          exact
           path="/station/:id"
           render={props => <Station id={props.match.params.id} />}
         />
+        <Route>
+          <About />
+        </Route>
       </Switch>
     </div>
   </div>

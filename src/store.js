a7a91976ec63f5d62f6aeddd256036f70276d270
Ole Barsch <ole.barsch@gmail.com>
2018-09-30 16:05:29 +0200

    fix mobile bug

--- src/store.js
+++ src/store.js
@@ -4,13 +4,17 @@ import rootReducer from './reducers';
 const initialState = {};
 
 const middleware = [thunk];
-
+let devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+if (process.env.NODE_ENV === 'prod' || process.env.NODE_ENV === 'production') {
+  devTools = a => a;
+}
 const store = createStore(
   rootReducer,
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    devTools
   )
 );
 

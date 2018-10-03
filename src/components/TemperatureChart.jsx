2f1e0f765d0502d4ac1ad34e8a626ff9774347b9
Ole Barsch <ole.barsch@gmail.com>
2018-10-03 21:11:49 +0200

    Add Footer

--- src/components/TemperatureChart.jsx
+++ src/components/TemperatureChart.jsx
@@ -26,7 +26,7 @@ class TemperatureChart extends Component {
 
   render() {
     return (
-      <div>
+      <div className="temperatureChart">
         {Object.keys(this.state.data).length > 0 && (
           <LineChart
             ytitle="°C"
--- src/containers/Routes.jsx
+++ src/containers/Routes.jsx
@@ -4,31 +4,31 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import Station from './Station';
 import About from './About';
-import NavBar from '../components/NavBar';
 import Error404 from './Error404';
 
-const Routes = () => (
-  <div>
-    <div className="grid">
-      <NavBar />
+import NavBar from '../components/NavBar';
+import Footer from '../components/Footer';
 
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route
-          exact
-          path="/station/:id"
-          render={props => <Station id={props.match.params.id} />}
-        />
-        <Route exact path="/about">
-          <About />
-        </Route>
-        <Route>
-          <Error404 />
-        </Route>
-      </Switch>
-    </div>
+const Routes = () => (
+  <div className="grid">
+    <NavBar />
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route
+        exact
+        path="/station/:id"
+        render={props => <Station id={props.match.params.id} />}
+      />
+      <Route exact path="/about">
+        <About />
+      </Route>
+      <Route>
+        <Error404 />
+      </Route>
+    </Switch>
+    <Footer />
   </div>
 );
 
